36b9e8e5d934742a56d390571f6a5343b52731ce
ElenaNUA <elena.nazarova0103@gmail.com>
2024-03-06 15:12:45 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print('                     -   -   -   -   -   -   -');
+  print('                 -   -                   -   -');
+  print('              -      -                -      -');
+  print('           -         -             -         -');
+  print('       -   -   -   -   -   -   -             -');
+  print('       -             -         -             -');
+  print('       -             -   -   -   -   -   -   -');
+  print('       -          -            -             -');
+  print('       -      -                -          - ');
+  print('       -   -                   -      - ');
+  print('       -   -   -   -   -   -   -  ');
+}
